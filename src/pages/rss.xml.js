6f44908d92256b6257d755763679e9e8608fd20d
1tpp <contact@1tpp.com>
2022-12-27 00:05:49 +0700

    chore: add blog details

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,19 @@
 import rss from '@astrojs/rss';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
 
+const blogImportResult = import.meta.glob('./blog/**/*.md', { eager: true });
+const blogs = Object.values(blogImportResult);
+
 export const get = () =>
 	rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: import.meta.env.SITE,
-		items: import.meta.glob('./blog/**/*.{md,mdx}'),
+		items: blogs.map((blog) => ({
+			link: blog.url,
+			title: blog.frontmatter.title,
+			pubDate: blog.frontmatter.pubDate,
+			description: blog.frontmatter.description,
+			tag: blog.frontmatter.tag,
+		}))
 	});
